6e249a7ff299c7d2ea30a9a878f55298d1c59e06
Guru <gurumurthy.ramamurthy@gmail.com>
2021-02-22 15:41:21 +0530

    Convereted into a maven project and tested it - commit 1

---
+++ MyApp100/src/com/ofss/TestProduct.java
@@ -0,0 +1,13 @@
+package com.ofss;
+
+// a class with the main method serves as an entry to the application
+public class TestProduct {
+
+	public static void main(String[] args) {
+		Stock st1=new Stock(100, "ORCL", 123.45);
+		System.out.println(st1);
+		System.out.println("_____________________");
+
+	}
+
+}
