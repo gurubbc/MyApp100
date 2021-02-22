cd9a9ff3f27b28b1f4bc759bbba97d8661d4b346
Guru <gurumurthy.ramamurthy@gmail.com>
2021-02-22 16:15:39 +0530

    Enhanced this app - added Customer object

--- MyApp100/src/com/ofss/TestProduct.java
+++ MyApp100/src/com/ofss/TestProduct.java
@@ -4,9 +4,17 @@ package com.ofss;
 public class TestProduct {
 
 	public static void main(String[] args) {
-		Stock st1=new Stock(100, "ORCL", 123.45);
-		System.out.println(st1);
-		System.out.println("_____________________");
+		Stock st1=new Stock(1, "ORCL", 1111.1);
+		Stock st2=new Stock(2, "IBM", 2111.1);
+		Stock st3=new Stock(3, "INFY", 3111.1);
+		//Create stock array
+		Stock stocks[]= {st1,st2,st3};
+		
+		Customer c=new Customer("Guru", stocks);
+		
+		// Print the Customer object
+		System.out.println(c);
+		
 
 	}
 
